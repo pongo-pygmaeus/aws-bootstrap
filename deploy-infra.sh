26196089d0e403956650cb4b83879fa2c64ac397
Maxwell Mathews <maxwellmathews@gmail.com>
2020-04-30 23:40:27 -0700

    adds codebuild/codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -4,7 +4,21 @@ REGION=us-west-2
 CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro
 
-  # Deploy the CloudFormation template
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+echo -e "\n\n=========== Deploying setup.yml ===========" 
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
@@ -22,3 +36,9 @@ if [ $? -eq 0 ]; then
     --profile awsbootstrap \
     --query "Exports[?Name=='InstanceEndpoint'].Value"
 fi
+
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
